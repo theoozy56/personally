54de0ac8b2eba9f06dcc797788b76058e8fcab78
Theo <theoozy56@gmail.com>
2025-02-15 23:25:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Hello, World! Theme Loaded Successfully");
+
+    // Cursor effect
+    const cursor = document.querySelector(".cursor");
+
+    document.addEventListener("mousemove", (e) => {
+        cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+    });
 });
